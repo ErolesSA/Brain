af71622bf6c88f9e29c131d7586ee8497c476f06
ErolesSA <68412071+ErolesSA@users.noreply.github.com>
2020-08-13 16:25:46 -0300

    Add files via upload

---
+++ registrohtml.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<h2></h2>
+<?php
+//recupera detalles del envio POST
+
+
+echo "el nombre es: " . $_POST['nombre'] . "<br>";
+echo "el nombre es: " . $_POST['clave'] . "<br>";
+
+?>
+</body>
+
+</html>
